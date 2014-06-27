6875c06d942e75001ef2df8be2d5af3be3b5cd42
Adam Clarke <adamclarkewastaken@gmail.com>
2014-06-27 16:33:49 -0500

    application controller rearrange

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,13 +2,15 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :null_session
-  after_action :set_csrf_cookie
 
-  def set_csrf_cookie
+  acts_as_token_authentication_handler_for User
+
+  after_action :set_csrf_header
+
+  def set_csrf_header
     if protect_against_forgery?
       headers['X-XSRF-TOKEN'] = form_authenticity_token
     end
   end
 
-  acts_as_token_authentication_handler_for User
 end
